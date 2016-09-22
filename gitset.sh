d4bea614a52b273914da49ff51e04220330c3f74
HajimeTokuyasu <p111026h@mail.kyutech.jp>
2016-09-22 17:59:30 +0900

    First commit.

---
+++ gitset.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git config --global alias.history log
+git config --global alias.br branch
+git config --global alias.stat status
+
---
+++ opera.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "press enter and hide alerts."
+opera & /dev/null 2>&1
+sleep 2s
+exit
+
